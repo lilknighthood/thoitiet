2ab55abe3fd9d7e51056f44a1c7b62a8dbf99696
lilknighthood <truongdinh93@gmail.com>
2025-07-04 13:20:13 +0700

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-pkg update && pkg upgrade -y
-pkg install root-repo -y
-pip install pycryptodome
-pip install psutil
-pkg install git tsu python wpa-supplicant pixiewps iw openssl -y
-git clone https://github.com/lilknighthood/thoitiet.git
-cd thoitiet
-chmod +x thoitiet.py
-sudo python thoitiet.py -i wlan0 -K
+* pkg update && pkg upgrade -y
+* pkg install root-repo -y
+* pip install pycryptodome
+* pip install psutil (nếu sài thoitietv2.py)
+* pkg install git tsu python wpa-supplicant pixiewps iw openssl -y
+* git clone https://github.com/lilknighthood/thoitiet.git
+* cd thoitiet
+* chmod +x thoitiet.py
+* sudo python thoitiet.py -i wlan0 -K
